47afb313dc88e1aade85fbaf79c6fbf3fa80b220
Aaron-Signer <aaronsigner1995@gmail.com>
2025-03-27 08:58:22 -0400

    Moved game script from main to simon_board script

--- scene/main/main.gd
+++ scene/main/main.gd
@@ -3,95 +3,28 @@ extends Node2D
 var final_button_queue: Array
 var removable_button_queue: Array
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	GameState.game_start.connect(start_game)
-	GameState.nav_main_menu.connect(load_main_menu)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	GameState.nav_main_menu.connect(load_main_menu)
+	GameState.game_start.connect(game_start)
+	GameState.game_end.connect(game_end)
+
 func _process(delta):
 	pass
-
-func simon_button_pressed(button_id: String):
-	var cur_button:SimonButton = removable_button_queue.pop_front()
-	if (cur_button != null):
-		if (cur_button.button_id != button_id):
-			game_end()
-		else:
-			if (removable_button_queue.front() == null):
-				GameState.update_score.emit(final_button_queue.size())
-				var new_simon_button = get_new_simon_button()
-				final_button_queue.push_back(new_simon_button)
-				removable_button_queue = final_button_queue.duplicate(true)
-				play_button_sequence()
-
-
-func _on_timer_timeout():
-	pass
-	#button_queue.push_back(button_list[randi_range(0, 3)])
-
-func get_new_simon_button() -> SimonButton:
-	var simon_buttons: Array = get_node("SimonBoard/SimonButtons").get_children()
 	
-	var new_simon_button: SimonButton = simon_buttons[randi_range(0, simon_buttons.size()-1)]
-	#new_simon_button.play_highlight()
-	#return new_simon_button.button_id
-	return new_simon_button
-
-func play_button_sequence():
-	reset_highlight_for_all_simon_buttons()
-	SimonBottonSignals.sequence_playing.emit(true)
 	
-	for simon_button:SimonButton in final_button_queue:
-		simon_button.highlight()
-		simon_button.enabled = false
-		
-		await get_tree().create_timer(1).timeout
-		simon_button.reset_highlight()
-		
-		await get_tree().create_timer(.5).timeout
-		
-	SimonBottonSignals.sequence_playing.emit(false)
-
-	
-func reset_highlight(simon_button: SimonButton):
-	#simon_button.reset_highlight()b
-	simon_button.enabled = true
-	
-func start_game():
+func game_start():
 	clear_children()
-	clear_simon_button_queues()
-	add_child(preload("res://scene/main/simon_board/simon_board.tscn").instantiate())
+	add_child(preload("res://scene/main/simon_board/simon_board.tscn").instantiate())	
 	
-	SimonBottonSignals.simon_button_pressed.connect(simon_button_pressed)
-	
-	var new_simon_button = get_new_simon_button()
-	
-	final_button_queue.push_back(new_simon_button)
-	removable_button_queue = final_button_queue.duplicate(true)
-	
-	play_button_sequence()
-	
-func clear_children():
-	for child in get_children():
-		child.queue_free()
-		
 func game_end():
 	clear_children()
 	add_child(preload("res://scene/main/menu/game_end/game_end.tscn").instantiate())
-		
-		
-func reset_highlight_for_all_simon_buttons(): 
-	var simon_buttons: Array = get_node("SimonBoard/SimonButtons").get_children()
-	
-	for simon_button in simon_buttons:
-		if simon_button.has_method("reset_highlight"):
-			simon_button.reset_highlight()
 
 func load_main_menu():
 	clear_children()
 	add_child(preload("res://scene/main/menu/main_menu/main_menu.tscn").instantiate())
-	
-func clear_simon_button_queues():
-	final_button_queue = []
-	removable_button_queue = []
+
+func clear_children():
+	for child in get_children():
+		child.queue_free()
--- scene/main/simon_board/simon_board.gd
+++ scene/main/simon_board/simon_board.gd
@@ -1,15 +1,89 @@
 extends Node2D
 
 @onready var score_label: Label = $ScoreLabel
+@onready var simon_buttons = $SimonButtons
+
+var final_button_queue: Array
+var removable_button_queue: Array
+
 
 func _ready():
 	GameState.update_score.connect(update_score)
+	start_game()
 
 func _process(delta):
 	pass
+
 
 func update_score(score: int):
 	score_label.text = str(score)
 
 	if score < 10:
 		score_label.text = "0" + score_label.text
+
+func simon_button_pressed(button_id: String):
+	var cur_button:SimonButton = removable_button_queue.pop_front()
+	if (cur_button != null):
+		if (cur_button.button_id != button_id):
+			GameState.game_end.emit()
+		else:
+			if (removable_button_queue.front() == null):
+				GameState.update_score.emit(final_button_queue.size())
+				var new_simon_button = get_new_simon_button()
+				final_button_queue.push_back(new_simon_button)
+				removable_button_queue = final_button_queue.duplicate(true)
+				play_button_sequence()
+
+
+func get_new_simon_button() -> SimonButton:
+	var simon_buttons: Array = simon_buttons.get_children()
+	
+	var new_simon_button: SimonButton = simon_buttons[randi_range(0, simon_buttons.size()-1)]
+#	TODO: Figure out reset function
+	#new_simon_button.play_highlight()
+	#return new_simon_button.button_id
+	return new_simon_button
+
+func play_button_sequence():
+	reset_highlight_for_all_simon_buttons()
+	SimonBottonSignals.sequence_playing.emit(true)
+	
+	for simon_button:SimonButton in final_button_queue:
+		simon_button.highlight()
+		simon_button.enabled = false
+		
+		await get_tree().create_timer(1).timeout
+		simon_button.reset_highlight()
+		
+		await get_tree().create_timer(.5).timeout
+		
+	SimonBottonSignals.sequence_playing.emit(false)
+
+	
+# TODO: May need to be removed
+func reset_highlight(simon_button: SimonButton):
+	#simon_button.reset_highlight()b
+	simon_button.enabled = true
+	
+func start_game():
+	clear_simon_button_queues()
+	
+	SimonBottonSignals.simon_button_pressed.connect(simon_button_pressed)
+	
+	var new_simon_button = get_new_simon_button()
+	
+	final_button_queue.push_back(new_simon_button)
+	removable_button_queue = final_button_queue.duplicate(true)
+	
+	play_button_sequence()
+		
+func reset_highlight_for_all_simon_buttons(): 
+	var simon_buttons: Array = simon_buttons.get_children()
+	
+	for simon_button in simon_buttons:
+		if simon_button.has_method("reset_highlight"):
+			simon_button.reset_highlight()
+	
+func clear_simon_button_queues():
+	final_button_queue = []
+	removable_button_queue = []
